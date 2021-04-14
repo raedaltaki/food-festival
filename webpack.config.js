8262f50fcbd53efc22b4ae3ea5ac2359e05a2285
raedaltaki <raed.simon@gmail.com>
2021-04-14 04:35:51 -0400

    add iphone icon

--- webpack.config.js
+++ webpack.config.js
@@ -56,7 +56,7 @@ module.exports = {
       inject: false,
       icons: [{
         src: path.resolve("assets/img/icons/icon-512x512.png"),
-        sizes: [96, 128, 192, 256, 384, 512],
+        sizes: [96, 128, 180, 192, 256, 384, 512],
         destination: path.join("assets", "icons")
       }]
     })
